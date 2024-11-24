e0b9ed29d021d4086d88fb00ac61c1a04b75d074
urlocalmimiii <163353523+urlocalmimiii@users.noreply.github.com>
2024-11-24 12:25:17 +0300

    Delete components/constants directory

--- components/constants/index.js
+++
@@ -1,6 +0,0 @@
-import icons from "./icons";
-import images from "./images";
-import illustrations from "./illustrations";
-import theme, { COLORS, SIZES, FONTS } from "./theme";
-
-export { icons, images, illustrations, theme, COLORS, SIZES, FONTS };
